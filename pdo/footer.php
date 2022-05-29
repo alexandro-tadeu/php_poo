f90242ef5fbe3f284276668c0cc3ff916f2b0791
alexandro-tadeu <82477172+alexandro-tadeu@users.noreply.github.com>
2022-05-29 15:40:57 -0300

    Delete pdo directory

--- pdo/footer.php
+++
@@ -1,6 +0,0 @@
-</body>
-<script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
-<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
-<script src="jquery-3.5.1.min.js"></script>
-<script src="javascript.min.js"></script>
-</html>
